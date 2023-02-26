465363e7ac1c4a8b7cb600d6ceb803e3fb44529a
Pavel Logunov <paveloguniv@gmail.com>
2023-02-26 09:34:37 +0900

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let result = '';
+  for(let i = 0; i < str.length; i++) {
+    for(let k = 0; k < bracketsConfig.length; k++) {
+      if(str[i] === bracketsConfig[k][0]) {
+        if(bracketsConfig[k][0] === bracketsConfig[k][1]) {
+          if(result[result.length - 1] === bracketsConfig[k][0]) {
+            result = result.substring(0, result.length - 1)
+          } else {
+            result += str[i];
+          }
+        } else {
+          result += str[i];
+        }
+      } else if(str[i] === bracketsConfig[k][1]) {
+        if(result[result.length - 1] === bracketsConfig[k][0]) {
+          result = result.substring(0, result.length - 1)
+        } else {
+          return false
+        }
+      }
+    }
+  } return result ? false : true
 }
